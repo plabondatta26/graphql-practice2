1f0f5e99187cd02c0939dca156a59a15c50c52c4
Plabon <plabondatta26@gmail.com>
2023-11-04 23:21:44 +0600

    middleware modified

--- django_graphql/execution_middleware.py
+++ django_graphql/execution_middleware.py
@@ -14,5 +14,31 @@ class GraphQLExecutionTimeMiddleware:
             execution_time = end_time - start_time
             content = json.loads(response.content)
             content['execution'] = {'executionTime': f'{execution_time} seconds'}
+            success_message = getattr(request, 'success_message', None)
+            error_message = getattr(request, 'error_message', None)
+            total_count = getattr(request, 'total_count', None)
+            if success_message:
+                content['success_message'] = success_message
+            if error_message:
+                content['error_message'] = error_message
+            if total_count:
+                content['total_count'] = total_count
+
+            response.content = json.dumps(content)
+        return response
+
+
+class GraphQLPaginationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if 'application/json' in response.get('Content-Type', ''):
+            content = json.loads(response.content)
+            for key, value in content:
+                if type(value) == list:
+                    pass
+                    # content['execution'] = {'executionTime': f'{execution_time} seconds'}
             response.content = json.dumps(content)
         return response
